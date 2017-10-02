809ef48881b5a4d2e62f70631f4ccb9a6e53caa1
Pedro Fregonesi Zamith <pedrozamith@hotmail.com.br>
2017-10-02 16:34:48 -0300

    Inclui o calculo do módulo pra organizar a recursividade

--- src/main.c
+++ src/main.c
@@ -2,12 +2,24 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, i, j, flag_print;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  for (i=1; i <= n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+	flag_print = 0;
+  
+	for (j=0; j<5; j++) {
+		if (i % a[j] == 0){
+			printf("%d", j);
+			flag_print = 1;
+		}	
+	}
+	if (flag_print == 0)
+		printf("-");
+	
+	printf("\n");
   }
 
   return 0;
